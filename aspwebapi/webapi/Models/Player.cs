f945d3a670880119559876b2dbf53af8bb3a2542
anthonyolores <tonyoolores@gmail.com>
2018-04-09 13:54:37 +1200

    added webapi project with phase 1 server code

---
+++ aspwebapi/webapi/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapi.Models
+{
+	public class Player
+	{
+		int movesCount = 0;
+		int [] moves = new int[5];
+		string name = "";
+		string symbol = "";
+		
+
+		public Player()
+		{
+		}
+
+		public Int32[] Moves { get => this.moves; set => this.moves = value; }
+		public Int32 MovesCount { get => this.movesCount; set => this.movesCount = value; }
+		public string Name { get => this.name; set => this.name = value; }
+		public String Symbol { get => this.symbol; set => this.symbol = value; }
+	}
+}
---
+++ aspwebapi/webapi/Models/Tictactoe.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapi.Models
+{
+	public class Tictactoe
+	{
+		Boolean status = false;
+		Player [] players = new Player[2];
+		string session = "";
+		int [,] pattern = new int[8,3] { 
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+			{1, 4, 7},
+			{2, 5, 8},
+			{3, 6, 9},
+			{1, 5, 9},
+			{3, 5, 7}
+		};
+
+		public Tictactoe(Player [] players){
+			this.players = players;
+		}
+		
+		public void addMove(int playerIndex, int moveIndex){		
+			this.players[playerIndex].Moves[this.players[playerIndex].MovesCount] = moveIndex;
+			this.players[playerIndex].MovesCount++;
+		}
+
+		public Boolean checkWin(int playerIndex){
+			
+			int movesCnt = this.players[playerIndex].MovesCount;
+			int matchCounter = 0;
+
+			if(movesCnt > 2){
+				for (int i = 0; i < this.pattern.GetLength(0) && matchCounter < 3; i++){	
+					matchCounter = 0;					
+					for(int x = 0; x < this.pattern.GetLength(1) && matchCounter < 3; x++){
+						for(int z = 0; z < movesCnt; z++){
+							if(this.players[playerIndex].Moves[z] == this.pattern[i, x]){
+								matchCounter++;
+								break;
+							}
+						}
+					}
+				}
+			
+			}
+
+			return matchCounter > 2? true : false;
+		}
+
+		public Boolean Status { get => this.status; set => this.status = value; }
+		public Player[] Players { get => this.players; set => this.players = value; }
+	}
+}
